b28fab91b7f50c3e3501ee51b848b3108c2319b1
btwise <tyq@qq.com>
2020-07-06 10:35:39 +0800

    update from github

--- Library/OcConfigurationLib/CheckSchema.py
+++ Library/OcConfigurationLib/CheckSchema.py
@@ -8,7 +8,7 @@ import re
 import sys
 
 if len(sys.argv) < 2:
-  print('Pass file to check')
+  print('传递文件进行检查')
   sys.exit(-1)
 
 with open(sys.argv[1], 'r') as f:
@@ -16,12 +16,12 @@ with open(sys.argv[1], 'r') as f:
   content = [l.strip() for l in f.readlines()]
   for i, l in enumerate(content):
     if l == 'OC_SCHEMA':
-      print('Checking schema {}'.format(re.match('^\w+', content[i+1]).group(0)))
+      print('检查架构 {}'.format(re.match('^\w+', content[i+1]).group(0)))
       prev = ''
       continue
     x = re.search('"([^"]+)"', l)
     if x:
       if x.group(1) < prev:
-        print('ERROR: {} succeeds {}'.format(prev, x.group(1)))
+        print('错误: {} 成功 {}'.format(prev, x.group(1)))
         sys.exit(1)
       prev = x.group(1)
